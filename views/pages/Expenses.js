8592a4be74ad1a28f4fa493e881bda4576d98902
David Wisecarver <46696828+dwisecar@users.noreply.github.com>
2021-05-24 13:35:12 -0700

    Update Expenses.js

--- views/pages/Expenses.js
+++ views/pages/Expenses.js
@@ -5,11 +5,19 @@ const Expenses = {
   allowAccess: async () => window.auth0Client.isAuthenticated(),
   render: async () => {
     const expenses = await expensesApi.getReports();
+    const dollars = await expensesApi.getDollars();
+    const cents = await expensesApi.getCents();
     const view = /*html*/ `
     <h1>Expense Report</h1>
     <p id="user-greet">Hello, ${window.user.name}</p>
     <p>These are your expenses:</p>
     ${await Table.render(expenses)}
+    `;
+    <p>These are your dollars:</p>
+    ${await Table.render(dollars)}
+    `;
+    <p>These are your cents:</p>
+    ${await Table.render(cents)}
     `;
     return view;
   },
