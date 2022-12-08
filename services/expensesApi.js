a618cfcd19b8085e0659d4d5d7ccd8ee506c804b
David Wisecarver <46696828+dwisecar@users.noreply.github.com>
2022-12-08 11:55:03 -0800

    Update expensesApi.js

--- services/expensesApi.js
+++ services/expensesApi.js
@@ -6,7 +6,9 @@ const expensesApi = {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
         },
       };
 
@@ -24,6 +26,10 @@ const expensesApi = {
           method: "GET",
           headers: {
           "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
           Authorization: `Bearer ${token}`,
           },
       };
